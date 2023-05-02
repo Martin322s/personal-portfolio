bc59068887292ea09345f3994e69a25dbc400d5d
Martin Sofroniev <marti.sofroniev0408@gmail.com>
2023-05-02 09:51:05 +0300

    contact component

---
+++ src/components/Contact/Contact.js
@@ -0,0 +1,13 @@
+import styles from "./styles/contact.module.css";
+
+const Contact = () => {
+    return (
+        <>
+            <h1 className={`${styles["contact-heading"]}`}>Contact me</h1>
+            <section className={`${styles["contact-section"]}`}>
+                
+            </section>
+        </>
+    );
+}
+export default Contact;
