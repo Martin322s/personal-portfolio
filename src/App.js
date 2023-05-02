1deafc7a3fde89de10882b6ca287c4abdb533930
Martin Sofroniev <marti.sofroniev0408@gmail.com>
2023-05-02 09:34:39 +0300

    footer component

--- src/App.js
+++ src/App.js
@@ -1,4 +1,5 @@
 import Education from "./components/Education/Education";
+import Footer from "./components/Footer/Footer";
 import Header from "./components/Header/Header";
 import About from "./components/Home/About";
 import Projects from "./components/Projects/Projects";
@@ -10,6 +11,7 @@ function App() {
             <About />
             <Projects />
             <Education />
+            <Footer />
         </>
     );
 }
