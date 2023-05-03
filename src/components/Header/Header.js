06458112137908bf4a21d616b410a1588fb18bf7
Martin Sofroniev <marti.sofroniev0408@gmail.com>
2023-05-03 17:11:30 +0300

    responsive header

--- src/components/Header/Header.js
+++ src/components/Header/Header.js
@@ -1,11 +1,12 @@
 import banner from "./images/banner-01.jpg";
 import styles from "./styles/header.module.css";
+import responsive from "./styles/responsive.module.css";
 
 const Header = () => {
     return (
         <header id="home" className={`${styles["heading"]}`}>
             <nav className={`${styles["navigation"]}`}>
-                <ul className={`${styles["navbar"]}`}>
+                <ul className={`${styles["navbar"]} ${responsive["navbar"]}`}>
                     <li><a className={`${styles["nav-link"]}`} href="#home">Home</a></li>
                     <li><a className={`${styles["nav-link"]}`} href="#about">About Me</a></li>
                     <li><a className={`${styles["nav-link"]}`} href="#projects">Projects</a></li>
@@ -16,11 +17,13 @@ const Header = () => {
             <div className={`${styles["container"]}`}>
                 <img className={`${styles["banner"]}`} src={banner} alt="banner" />
                 <div className={`${styles["gradient"]}`}></div>
-                <h1 className={`${styles["heading-title"]}`}>Welcome to my personal portfolio!</h1>
-                <p className={`${styles["welcoming-msg"]}`}>
-                    I'm an enthusiastic junior front-end developer with a desire to grow 
-                    and improve every day. I'm ready to learn and develop my skills.
-                </p>
+                <div className={`${responsive["container-text"]}`}>
+                    <h1 className={`${styles["heading-title"]}`}>Welcome to my personal portfolio!</h1>
+                    <p className={`${styles["welcoming-msg"]} ${responsive["welcoming-msg"]}`}>
+                        I'm an enthusiastic junior front-end developer with a desire to grow
+                        and improve every day. I'm ready to learn and develop my skills.
+                    </p>
+                </div>
             </div>
         </header>
     );
